8a935081ad55e8ffd63a06b01ba673af7b1606c2
RyanD1996 <ryan.drohan@outlook.com>
2019-03-21 16:56:36 +0000

    commit

--- VirtualReality_MCW/Assets/Scipts/deleteObj.cs
+++ VirtualReality_MCW/Assets/Scipts/deleteObj.cs
@@ -6,9 +6,12 @@ public class deleteObj : MonoBehaviour
 {
     public GameObject gameObject;
     public string scenename;
-    public void OnMouseDown()
+
+    public void OnMouseDown() 
+
     {
         Application.LoadLevel(scenename);
+ 
         Destroy(gameObject);
     }
 }
