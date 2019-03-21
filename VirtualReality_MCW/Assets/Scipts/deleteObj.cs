abe6dd503249f93ab7ea38bc398ce8ccc41e6d31
Gregor Adams <gregoradamsuni@gmail.com>
2019-03-21 10:58:47 +0000

    Script
    Button Script

---
+++ VirtualReality_MCW/Assets/Scipts/deleteObj.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deleteObj : MonoBehaviour
+{
+    public GameObject gameObject;
+    public string scenename;
+    public void OnMouseDown()
+    {
+        Application.LoadLevel(scenename);
+        Destroy(gameObject);
+    }
+}
